9191dfbe50ac82cf5b50db54e131fe167ad2c4ed
SofZOO <77208676+SofZOO@users.noreply.github.com>
2021-05-16 11:07:21 +0200

    Structure Joueur et parametre
    Implémentation des 2 structures dans le module GameStorage.vb

--- GameStorage.vb
+++ GameStorage.vb
@@ -8,6 +8,14 @@
     ' - nbre max de carrés d'une partie
     ' - temps min associé
     ' - cumul du temps de jeu
+    Public Structure Joueur
+        Public Nom As String
+        Public nbrMaxCarréTrouvés As Integer
+        Public tempsMin As Integer
+        Public cumulTmpJeu As Integer
+    End Structure
+
+
 
     ' Stockage de paramètres : 
     ' - réglage du temps alloué au joueur (0 pour désactiver) -> via un picker
@@ -15,4 +23,17 @@
     ' - Chemin du fichier de sauvegarde (!) -> chemin
     ' - Le pause sera par défaut
     ' - (facultatif) CheaterMode qui désactive le random et les vérifications -> code konami
+
+    Public Structure Paramètres
+        Public tempsMax As Integer
+        Public thèmeUtilisé As String
+        Public cheminSauvegarde As String
+        Public pause As Boolean
+    End Structure
+
+
+    Private tabJoueurs As Joueur()
+
+
+
 End Module
